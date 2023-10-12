88c1addf1678235466961a596513a92677f61902
InnaLik <110997246+InnaLik@users.noreply.github.com>
2023-10-12 17:11:34 +0300

    2.5 Шаблонный тег url

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -2,35 +2,44 @@ from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpRespons
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.template.defaultfilters import slugify
-menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
+
+menu = [{'title': 'О сайте', 'url_name': 'about'},
+        {'title': 'Добавить статью', 'url_name': 'add_page'},
+        {'title': 'Обратная связь', 'url_name': 'contact'},
+        {'title': 'Войти', 'url_name': 'login'}]
 data_db = [{'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_publisher': True},
            {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_publisher': False},
            {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_publisher': True}]
+
+
 def index(request):
-    #если в шаблоне хотим отобразить две переменные - то ,<p>{{первая переменная}} {{вторая переменная}}</p>
+    # если в шаблоне хотим отобразить две переменные - то ,<p>{{первая переменная}} {{вторая переменная}}</p>
     data = {'title': 'главная страница',
             'menu': menu,
             'posts': data_db}
     return render(request, 'women/index.html', context=data)
 
+
 def about(request):
     data = {'title': 'О сайте'}
     return render(request, 'women/about.html', data)
 
-def categories(request, cat_id):
-    return HttpResponse(f'<h1>Статьи по категориям</h1><p>id: {cat_id}</p>')
 
-def categories_by_slug(request, cat_slug):
-    return HttpResponse(f'<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>')
+def show_post(request, post_id):
+    return HttpResponse(f'Отображение статьи с id= {post_id}')
 
 
-def archive(request, year):
-    if year > 2023:
-        uri = reverse('cats', args=['music'])
-        return HttpResponseRedirect('home')
-    return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
+def addpage(request):
+    return HttpResponse('Добавление статьи')
+
+
+def contact(request):
+    return HttpResponse('Обратная связь')
+
+
+def login(request):
+    return HttpResponse('Авторизация')
+
 
 def page_not_found(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
-
-

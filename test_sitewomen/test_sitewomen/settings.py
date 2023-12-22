9652514903e3ebda78ce8922fd8f2fe9ca0dc49e
InnaLik <110997246+InnaLik@users.noreply.github.com>
2023-12-22 21:16:08 +0300

    22/12/2023 17.7 3 minutes

--- test_sitewomen/test_sitewomen/settings.py
+++ test_sitewomen/test_sitewomen/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5x$i&!=6xbu1f#alsg1wg)pte6d5-ze6!+7#r+f7n939pu^b-#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -122,3 +122,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
--- test_sitewomen/women_test/views.py
+++ test_sitewomen/women_test/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render
 
 
@@ -12,8 +12,16 @@ def categories(request, cat_id):
 
 
 def categories_by_slug(request, cat_slug):
+    if request.POST:
+        print(request.POST)
     return HttpResponse(f'<h1>Статьи по категориям</h1><p>: {cat_slug}')
 
 
 def archive(request, year):
+    if year > 2023:
+        raise Http404()
     return HttpResponse(f'{year}')
+
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound('Страница не найдена')

2b3b03504ebcb6ed054623ec17066029bacf2868
InnaLik <110997246+InnaLik@users.noreply.github.com>
2024-01-19 22:33:15 +0300

    3.1 3.2 3.3 without tasks 19 01 2024

--- test_sitewomen/test_sitewomen/settings.py
+++ test_sitewomen/test_sitewomen/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'django_extensions',
     'women_test.apps.WomenTestConfig',
     'django.contrib.admin',
     'django.contrib.auth',

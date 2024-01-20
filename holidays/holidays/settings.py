f0b66f28194978215db4412f5f70084e2698b04d
InnaLik <110997246+InnaLik@users.noreply.github.com>
2024-01-20 23:30:06 +0300

    only create table 20 01 2024

--- holidays/holidays/settings.py
+++ holidays/holidays/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8c8v71)4cp^_i5)$un*-mivsa_&e%n1z$3q%(t%5jv(^c4)9a!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
--- holidays/main_page/models.py
+++ holidays/main_page/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Holiday(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    day = models.DateField()
+    international = models.BooleanField(default=False)
+    worldwide = models.BooleanField(default=False)
+    on_russian = models.BooleanField(default=False)
+    other_country = models.BooleanField(default=False)
+    fill_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)

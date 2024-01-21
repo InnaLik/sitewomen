e677c7da212a814e9a5af7cbc87e9f80f97a1b21
InnaLik <110997246+InnaLik@users.noreply.github.com>
2024-01-21 23:16:37 +0300

    21 01 2024 3.2 all

--- holidays/main_page/models.py
+++ holidays/main_page/models.py
@@ -11,3 +11,30 @@ class Holiday(models.Model):
     other_country = models.BooleanField(default=False)
     fill_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
+
+
+class Users(models.Model):
+    telegram_id = models.IntegerField()
+    name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
+    fill_date = models.DateTimeField(auto_now_add=True)
+
+
+# если пользователей хочет чтобы ему пришло уведомление об определенном празднике
+# дату, наименование и описание праздника пользователь вводит сам
+class OneNotifications(models.Model):
+    telegram_id = models.IntegerField()
+    name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
+    date_holiday = models.DateTimeField()
+    name_holiday = models.CharField(max_length=100)
+    desc_holiday = models.TextField(max_length=600)
+    fill_date = models.DateTimeField(auto_now_add=True)
+
+
+# обратная связь
+class Feedback(models.Model):
+    id_users = models.IntegerField()
+    telegram_id = models.IntegerField(blank=True)
+    desc = models.TextField(max_length=1000)
+

e7d5e34a03359711a5bc2a2775552fa415b272bb
InnaLik <110997246+InnaLik@users.noreply.github.com>
2024-01-27 21:22:44 +0300

    fds

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('addpage/', views.addpage, name='add_page'),
     path('contact/', views.contact, name='contact'),
     path('login/', views.login, name='login'),
+    path('post'), views.dfd),
     path('category/<int:cat_id>/', views.show_category, name='category')
 
 ]

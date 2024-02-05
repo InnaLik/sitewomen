731c96a1f416fca45db7ef8ecdcf8194bb456fdd
InnaLik <110997246+InnaLik@users.noreply.github.com>
2024-02-05 21:28:22 +0300

    05 02 2023

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -13,18 +13,17 @@ class PublishedManager(models.Manager):
 class Women(models.Model): #именно это наследование превращает наш в класс в обхект модели
     #класс перечислений, должны быть именно кортежами
     class Status(models.IntegerChoices):
-        DRAFT = 0, 'Черновик'
-        PUBLISHED = 1, 'Опубликовано'
+        DRAFT = 0, 'черновик'
+        PUBLISHED = 1, 'опубликовано'
     slug = models.SlugField(max_length=255, db_index=True, unique=True)
     title = models.CharField(max_length=255) #текстовые поля
     content = models.TextField(blank=True) #blank позволяет нам не задавать значение поля при записи таблицы
     time_create = models.DateTimeField(auto_now_add=True) #auto автоматически будет заполнять поле, но только в момент первого появления данной записи
     time_update = models.DateTimeField(auto_now=True) #меняется каждый раз при записи в базу данных (автоматически)
-    is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
+    is_published = models.BooleanField(choices=Status.choices, default=Status.PUBLISHED)
 
     published = PublishedManager()
     objects = models.Manager()
-
     #чтобы в оболочке при вызове данных из таблицы красиво показывалось
     def __str__(self):
         return self.title

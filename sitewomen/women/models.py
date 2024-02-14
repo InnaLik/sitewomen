ef45a9f03e6e4c5941947d2271e420013e342d6f
InnaLik <110997246+InnaLik@users.noreply.github.com>
2024-02-15 00:17:13 +0300

    15 02 2024

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -45,6 +45,9 @@ class Category(models.Model):
     slug = models.SlugField(max_length=255, unique=True, db_index=True)
 
 
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'cat_slug': self.slug})
+
     def __str__(self):
         return self.name
 
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -6,7 +6,7 @@ from django.template.defaultfilters import slugify
 
 
 # после перерыва 19.12.2023 37 минут
-from women.models import Women
+from women.models import Women, Category
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Добавить статью", 'url_name': 'add_page'},
@@ -24,13 +24,6 @@ data_db = [
     {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
     {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
 ]
-
-cats_db = [
-    {'id': 1, 'name': 'Актрисы'},
-    {'id': 2, 'name': 'Певицы'},
-    {'id': 3, 'name': 'Спортсменки'},
-]
-
 
 def index(request):
     posts = Women.published.all()
@@ -75,11 +68,13 @@ def page_not_found(request, exception):
 
 
 # очень сложно, надо много раз прослушать тему, чтобы понять
-def show_category(request, cat_id):
+def show_category(request, cat_slug):
+    category = get_object_or_404(Category, slug=cat_slug)
+    posts = Women.published.filter(cat_id=category.pk)
     data = {
-        'title': 'Отображение по рубрикам',
+        'title': f'Рубрика {category.name}',
         'menu': menu,
-        'posts': data_db,
-        'cat_selected': cat_id,
+        'posts': posts,
+        'cat_selected': category.pk,
     }
     return render(request, 'women/index.html', context=data)

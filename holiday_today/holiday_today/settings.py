408cddd695eb65a81ba88441eae39f0432ad3edb
InnaLik <110997246+InnaLik@users.noreply.github.com>
2024-03-01 23:21:42 +0300

    add key

--- holiday_today/holiday_today/settings.py
+++ holiday_today/holiday_today/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from secret_key import key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=2#2l5@!s==f)zb)3^0!ax%fzn!w1r2$18b^sly#wb4)ntt2u2'
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

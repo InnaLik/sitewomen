e3c4927931c3451cdd65a037ca49fdfa7b4c963b
InnaLik <110997246+InnaLik@users.noreply.github.com>
2024-03-16 19:03:11 +0300

    добавление собственного фильтра

--- sitewomen/women/admin.py
+++ sitewomen/women/admin.py
@@ -4,6 +4,25 @@ from .models import Women, Category
 admin.site.site_header = 'Панель администрирования'
 
 admin.site.index_title = 'Известные женщины мира'
+
+
+class MariedFilter(admin.SimpleListFilter):
+    title = 'Статус женщин'
+    parameter_name = 'status'
+
+    # request - объект запроса, этот метод возвращает список из возможных значений параметра статус
+    def lookups(self, request, model_admin):
+        return [('married', 'Замужем'),
+                ('single', 'Не замужем')]
+
+    # queryset - набор записей, из которых мы можем отбирать для фильтра те или иные записи
+    def queryset(self, request, queryset):
+        # self.value - возвращает значение параметра статус
+        if self.value() == 'married':
+            return queryset.filter(husb__isnull=False)
+        return queryset.filter(husb__isnull=True)
+
+
 @admin.register(Women)
 class WomenAdmin(admin.ModelAdmin):
     list_display = ('title', 'time_create', 'is_published', 'cat', 'brief_info')
@@ -17,18 +36,21 @@ class WomenAdmin(admin.ModelAdmin):
     # пагинация списка - максимальное количество статей для отображения на странице
     list_per_page = 20
     actions = ['set_published', 'set_draft']
+    # указываем список полей, по которым будет осуществляться поиск
+    search_fields = ['title__startswith', 'cat__name']
+    # панель фильтрации
+    list_filter = [MariedFilter, 'cat__name', 'is_published']
 
     @admin.display(description='Краткое описание', ordering='content')
     def brief_info(self, women: Women):
         return f'Описание {len(women.content)} символов'
 
     # для того чтобы действие было делать статьи опубликованными
-    #queryset - это коллекция объектов
+    # queryset - это коллекция объектов
     @admin.action(description='Опубликовать выбранные записи')
     def set_published(self, request, queryset):
         count = queryset.update(is_published=Women.Status.PUBLISHED)
         self.message_user(request, f'Изменено {count} записей')
-
 
     @admin.action(description='Снять с публикации')
     def set_draft(self, request, queryset):

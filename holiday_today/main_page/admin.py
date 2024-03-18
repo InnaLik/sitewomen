218e5d5d7d20de157aff714bd552253a04a73405
InnaLik <110997246+InnaLik@users.noreply.github.com>
2024-03-18 19:56:32 +0300

    admin-panel

--- holiday_today/main_page/admin.py
+++ holiday_today/main_page/admin.py
@@ -11,15 +11,22 @@ admin.site.index_title = 'Праздники'
 @admin.register(Holiday)
 class HolidayAdmin(admin.ModelAdmin):
     list_display = ['name', 'international', 'worldwide', 'ordinary_holiday', 'slug', 'get_country']
-    list_display_link = ['name']
+    list_display_link = ['name', 'slug']
+    list_editable = ['international', 'worldwide', 'ordinary_holiday']
     actions = ['set_international']
+    # указать список полей для фильтра
+    # панель фильтрации сделать
 
     @admin.action(description='Сделать международными')
     def set_international(self, request, queryset):
+        # количество измененных записей
         count = queryset.update(international=True)
         self.message_user(request, f'{count} праздников сделаны международными')
 
     @admin.display(description='Страна')
     def get_country(self, holiday: Holiday):
         return [i.title for i in holiday.country.all()]
+
+
+
 

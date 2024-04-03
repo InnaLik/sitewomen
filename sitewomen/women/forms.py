0a689a99346e63b749d0e8b4be58baa8d77a51dc
InnaLik <110997246+InnaLik@users.noreply.github.com>
2024-04-03 18:40:26 +0300

    создание собственных валидаторов и классов валидаторов

--- sitewomen/women/forms.py
+++ sitewomen/women/forms.py
@@ -5,6 +5,18 @@ from django.utils.deconstruct import deconstructible
 
 from .models import Category, Husband
 
+@deconstructible
+class ContentValidator:
+    def __init__(self, message=None):
+        self.message = message if message else 'Недопустимо использование таких словосочетаний'
+
+
+    def __call__(self, value,  *args, **kwargs):
+        if value == 'жы':
+            raise ValidationError(message=self.message)
+        return value
+
+
 class AddPostForm(forms.Form):
     # атрибуты полей формы textinput - это стиль оформления
     title = forms.CharField(max_length=255, min_length=5,
@@ -18,11 +30,18 @@ class AddPostForm(forms.Form):
                                MaxLengthValidator(100)
                            ])
     # поле ввода для текста
-    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 5}), required=False, label="Контент")
+    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 5}), required=False, label="Контент",
+                              validators=[ContentValidator()])
     is_published = forms.BooleanField(required=False, label="Статус", initial=True)
     # отображение в виде выпадающего списка
     cat = forms.ModelChoiceField(queryset=Category.objects.all(), label="Категории", empty_label="Категория не выбрана")
     husb = forms.ModelChoiceField(queryset=Husband.objects.all(), required=False, label="Муж", empty_label="Не замужем")
+
+    def clean_slug(self):
+        value = self.cleaned_data['slug']
+        if '-' in value:
+            raise ValidationError(message='Дефисы недопустимы')
+        return value
 
     def clean_title(self):
         title = self.cleaned_data['title']

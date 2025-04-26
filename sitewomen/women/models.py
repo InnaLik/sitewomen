8dfa08c1a05998ca8ed7d53cd69a1718b4e64138
InnaLik <liksakova@list.ru>
2025-04-26 18:08:51 +0300

    Revert "26 01 2024"
    This reverts commit 2b3b21bce5426920f91574a643b62b942d2f4980, reversing
    changes made to db2ff6654c57cad226a56e8e5fb6d1b5a1cdc056.

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -7,11 +7,7 @@ from django.urls import reverse
 
 class Women(models.Model): #именно это наследование превращает наш в класс в обхект модели
     title = models.CharField(max_length=255) #текстовые поля
-<<<<<<< HEAD
     slug = models.SlugField(max_length=255, unique=True, db_index=True)
-=======
-    slug = models.SlugField(max_length=255, blank=True, db_index=True, default='')
->>>>>>> 34c923ab9f1d774ab4538e3c993e2eb01c75c4c5
     content = models.TextField(blank=True) #blank позволяет нам не задавать значение поля при записи таблицы
     time_create = models.DateTimeField(auto_now_add=True) #auto автоматически будет заполнять поле, но только в момент первого появления данной записи
     time_update = models.DateTimeField(auto_now=True) #меняется каждый раз при записи в базу данных (автоматически)
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -4,10 +4,7 @@ from django.urls import reverse
 from django.template.loader import render_to_string
 from django.template.defaultfilters import slugify
 
-<<<<<<< HEAD
 # после перерыва 19.12.2023 37 минут
-=======
->>>>>>> 34c923ab9f1d774ab4538e3c993e2eb01c75c4c5
 from women.models import Women
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
@@ -49,7 +46,6 @@ def about(request):
     return render(request, 'women/about.html', {'title': 'О сайте', 'menu': menu})
 
 
-<<<<<<< HEAD
 def show_post(request, post_slug):
     post = get_object_or_404(Women, slug=post_slug)
     data = {
@@ -57,16 +53,6 @@ def show_post(request, post_slug):
         'menu': menu,
         'post': post,
         'cat_selected': 1}
-=======
-
-def show_post(request, post_id):
-    # читаем одну запись
-    post = get_object_or_404(Women, pk=post_id)
-    data = {'title': post.title,
-            'menu': menu,
-            'post': post,
-            'cat_selected': 1}
->>>>>>> 34c923ab9f1d774ab4538e3c993e2eb01c75c4c5
     return render(request, 'women/post.html', data)
 
 

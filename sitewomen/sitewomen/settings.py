b50e26a2f5e197e3fa5b2ae7c0f77246f61e2735
InnaLik <liksakova@list.ru>
2025-04-26 18:03:55 +0300

    Revert "работа с изображениями"
    This reverts commit 101c9534f27986c33305815df71fb7a7333baa69.

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -133,7 +133,6 @@ STATICFILES_DIRS = [
                     ]
 
 MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = '/media/'
 
 # STATICFILES_DIRS = [BASE_DIR / 'sitewomen/static']
 # Default primary key field type
--- sitewomen/sitewomen/urls.py
+++ sitewomen/sitewomen/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from sitewomen import settings
 from women.views import *
 
 urlpatterns = [
@@ -26,8 +24,6 @@ urlpatterns = [
     path('', include('women.urls')),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = page_not_found
 

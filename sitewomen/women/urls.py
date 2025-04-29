3283d0fbc9ffee274ea6a6e67a4a1b21fd486296
InnaLik <liksakova@list.ru>
2025-04-29 23:31:17 +0300

    1.6

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -4,6 +4,5 @@ from women import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('cats/<slug:category_slug>/', views.categories, name='categories')
 ]
-
-
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 def index(request):
     """Главная страница сайта."""
     return HttpResponse("Главная страница")
+
+def categories(request, category_slug:str):
+    return HttpResponse(f"{category_slug}")
